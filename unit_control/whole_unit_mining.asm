29a1d8be284785eb95593ac0b424196cccf195fd
zinet84 <simdh2@naver.com>
2025-06-05 02:03:33 +0900

    코드 수정

--- unit_control/whole_unit_mining.asm
+++ unit_control/whole_unit_mining.asm
@@ -12,20 +12,20 @@ sensor uid @unit @flag
 jump 28 notEqual uid select_uid
 ulocate ore core true resource outx outy found building
 sensor ti @unit @totalItems
-jump 18 equal ti 0
+jump 20 equal ti 0
 sensor fi @unit @firstItem
-jump 18 equal fi resource
+jump 20 equal fi resource
 ucontrol move tx ty 0 0 0
 ucontrol itemDrop target 999 0 0 0
-jump 26 always found true
+jump 28 always found true
 sensor total @unit @totalItems
 sensor capa @unit @itemCapacity
-jump 24 notEqual total capa
+jump 26 notEqual total capa
 ucontrol approach tx ty 8 0 0
 ucontrol itemDrop target 999 0 0 0
-jump 26 always found true
+jump 28 always found true
 ucontrol approach outx outy 8 0 0
 ucontrol mine outx outy 0 0 0
 op add i i 1
-jump 7 lessThanEq i 24
+jump 9 lessThanEq i 24
 end
