9a1ebefb7e199827abc08183ee398683aa6e7013
zinet84 <simdh2@naver.com>
2025-06-04 22:03:58 +0900

    코드 수정

--- unit_control/assign_unit_id.asm
+++ unit_control/assign_unit_id.asm
@@ -1,13 +1,12 @@
 print "Variable Area"
+set unit @mono
 set max_unit 24
 print "Execution Area"
 set i 1
-ubind @mono
+ubind unit
 sensor curr_uid @unit @flag
-jump 6 notEqual curr_uid 0
+jump 9 notEqual curr_uid 0
 ucontrol flag i 0 0 0 0
 op add i i 1
-sensor result @unit @flag
-printflush message1
-jump 2 lessThanEq i max_unit
+jump 5 lessThanEq i max_unit
 end
